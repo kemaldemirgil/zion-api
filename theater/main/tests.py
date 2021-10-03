7c5692d46f56a703c01c45a4ff4fca1b3f567a54
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-10-03 12:07:38 -0400

    adding room test & room model

--- theater/main/tests.py
+++ theater/main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Room
 
-# Create your tests here.
+
+class RoomTest(TestCase):
+  """ Test module for Room model """
+
+  def setUp(self):
+    Room.objects.create(name='Room-1')
+    Room.objects.create(name='Room-2')
+
+  def test_Room(self):
+    room_1 = Room.objects.get(name='Room-1')
+    room_2 = Room.objects.get(name='Room-2')
+    self.assertEqual(
+      room_1.__str__(), "Room-1")
+    self.assertEqual(
+      room_2.__str__(), "Room-2")
