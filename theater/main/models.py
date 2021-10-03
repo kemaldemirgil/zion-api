9fdd1b571cded154d08306c5a7bae7877a407669
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-10-03 13:00:28 -0400

    adding showtime test & showtime model

--- theater/main/models.py
+++ theater/main/models.py
@@ -15,3 +15,14 @@ class Movie(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class ShowTime(models.Model):
+  """ ShowTime model """
+  movie_name = models.ForeignKey(Movie, on_delete=models.CASCADE, null=False)
+  room_name = models.ForeignKey(Room, on_delete=models.CASCADE, null=False, related_name="show_time")
+  time = models.CharField(max_length = 100, null=False, unique=False)
+  seats = models.IntegerField(default=300)
+
+  def __str__(self):
+    return f"{self.movie_name} will be playing at {self.time} with {self.seats} capacity."
--- theater/main/tests.py
+++ theater/main/tests.py
@@ -1,15 +1,14 @@
 from django.test import TestCase
-from .models import Room, Movie
+from .models import Room, Movie, ShowTime
 
 
 class RoomTest(TestCase):
-  """ Test module for Room model """
-
   def setUp(self):
     Room.objects.create(name='Room-1')
     Room.objects.create(name='roomtwo')
 
   def test_Room(self):
+    """ Rooms that can be created are correctly identified """
     room_1 = Room.objects.get(name='Room-1')
     room_2 = Room.objects.get(name='roomtwo')
     self.assertEqual(
@@ -19,16 +18,32 @@ class RoomTest(TestCase):
 
   
 class MovieTest(TestCase):
-  """ Test module for Movie model """
-
   def setUp(self):
     Movie.objects.create(name='Godfather')
     Movie.objects.create(name='Pulp Fiction')
 
   def test_Movie(self):
+    """ Movies that can be created are correctly identified """
     movie_godfather = Movie.objects.get(name='Godfather')
     movie_pulpfiction = Movie.objects.get(name='Pulp Fiction')
     self.assertEqual(
       movie_godfather.__str__(), "Godfather")
     self.assertEqual(
       movie_pulpfiction.__str__(), "Pulp Fiction")
+
+
+class ShowTimeTest(TestCase):
+  def setUp(self):
+    self.movie_name = Movie.objects.create(name="Kill Bill")
+    self.room_name = Room.objects.create(name="Room-3")
+    self.time = "8.00-11.00"
+    self.seats = 4
+    
+
+  def test_ShowTime(self):
+    """ Show Times that can be created are correctly identified """
+    self.assertEqual(self.movie_name.name, "Kill Bill")
+    self.assertEqual(self.room_name.name, "Room-3")
+    self.assertEqual(self.time, "8.00-11.00")
+    self.assertEqual(self.seats, 4)
+    
